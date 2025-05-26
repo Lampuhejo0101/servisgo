950069d40c525888c564c2864b2c984648e3d7cc
Lampuhejo0101 <lampuhejo0007@gmail.com>
2025-05-26 16:46:04 +0700

    auth.js

--- script/auth.js
+++ script/auth.js
@@ -1,1 +1,9 @@
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Login sukses
+    window.location.href = "admin.html";
+  })
+  .catch((error) => {
+    alert("Login gagal: " + error.message);
+  });
 // Custom auth scripts if needed
